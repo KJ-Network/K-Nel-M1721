0728ca21dabbcdd753fcb2c9afdb87828f561d1e
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-04 20:46:18 -0700

    sched/fair: Always update CPU capacity when load balancing
    Limiting CPU capacity updates, which are quite cheap, results in worse
    balancing decisions during opportunistic balancing (e.g., SD_BALANCE_WAKE).
    This causes opportunistic placement decisions to be skewed using stale CPU
    capacity data, and when a CPU isn't idling much, its capacity suffers from
    even more staleness since the only exception to the 100 ms capacity update
    ratelimit is a CPU exiting idle.
    
    Since the capacity updates are cheap, always do it when load balancing in
    order to improve opportunistic task placement decisions.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Rsyd <rasyid2007alkautsar@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9116,11 +9116,6 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 	struct sched_domain *child = sd->child;
 	struct sched_group *group, *sdg = sd->groups;
 	unsigned long capacity, max_capacity, min_capacity;
-	unsigned long interval;
-
-	interval = msecs_to_jiffies(sd->balance_interval);
-	interval = clamp(interval, 1UL, max_load_balance_interval);
-	sdg->sgc->next_update = jiffies + interval;
 
 	/*
 	 * When there is only 1 CPU in the sched group of a higher
@@ -9578,10 +9573,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 		if (local_group) {
 			sds->local = sg;
 			sgs = &sds->local_stat;
-
-			if (env->idle != CPU_NEWLY_IDLE ||
-			    time_after_eq(jiffies, sg->sgc->next_update))
-				update_group_capacity(env->sd, env->dst_cpu);
+			update_group_capacity(env->sd, env->dst_cpu);
 		}
 
 		update_sg_lb_stats(env, sg, load_idx, local_group, sgs,
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1165,7 +1165,6 @@ struct sched_group_capacity {
 	unsigned long capacity;
 	unsigned long max_capacity; /* Max per-cpu capacity in group */
 	unsigned long min_capacity; /* Min per-CPU capacity in group */
-	unsigned long next_update;
 	int imbalance; /* XXX unrelated to capacity but shared group state */
 
 	unsigned long cpumask[0]; /* iteration mask */
