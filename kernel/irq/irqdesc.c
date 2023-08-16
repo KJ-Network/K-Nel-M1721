098df1cb53cf516510033874b0c41456e2c7d085
Sultan Alsawaf <sultan@kerneltoast.com>
2023-08-15 19:57:28 -0700

    kernel: Introduce SBalance IRQ balancer
    This is a simple IRQ balancer that polls every X number of milliseconds and
    moves IRQs from the most interrupt-heavy CPU to the least interrupt-heavy
    CPUs until the heaviest CPU is no longer the heaviest. IRQs are only moved
    from one source CPU to any number of destination CPUs per balance run.
    Balancing is skipped if the gap between the most interrupt-heavy CPU and
    the least interrupt-heavy CPU is below the configured threshold of
    interrupts.
    
    The heaviest IRQs are targeted for migration in order to reduce the number
    of IRQs to migrate. If moving an IRQ would reduce overall balance, then it
    won't be migrated.
    
    The most interrupt-heavy CPU is calculated by scaling the number of new
    interrupts on that CPU to the CPU's current capacity. This way, interrupt
    heaviness takes into account factors such as thermal pressure and time
    spent processing interrupts rather than just the sheer number of them. This
    also makes SBalance aware of CPU asymmetry, where different CPUs can have
    different performance capacities and be proportionally balanced.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ic7eeebc979baf72f8bb463178585370d649e57f7
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -392,6 +392,7 @@ static struct irq_desc *alloc_desc(int irq, int node, unsigned int flags,
 	desc_set_defaults(irq, desc, node, affinity, owner);
 	irqd_set(&desc->irq_data, flags);
 	kobject_init(&desc->kobj, &irq_kobj_type);
+	sbalance_desc_add(desc);
 
 	return desc;
 
@@ -422,6 +423,7 @@ static void free_desc(unsigned int irq)
 {
 	struct irq_desc *desc = irq_to_desc(irq);
 
+	sbalance_desc_del(desc);
 	unregister_irq_proc(irq, desc);
 
 	/*
