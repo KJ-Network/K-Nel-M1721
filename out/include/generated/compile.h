215e125e5ac05b987aad6db364aa96ebc1f83fe4
KNKJ55 <kskjprotonemail@proton.me>
2024-01-22 15:46:57 +0800

    update-r-1.2-sources

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 22 11:21:34 CST 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILER "clang version 11.0.0"
