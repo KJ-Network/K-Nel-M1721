df6f9e8231b1f11fb4515e8cee849e3c218813f6
KNKJ55 <kskjprotonemail@proton.me>
2024-01-29 13:59:36 +0800

    test

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 22 11:21:34 CST 2024"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jan 27 16:02:25 CST 2024"
 #define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILE_HOST "M2012K11AC"
 #define LINUX_COMPILER "clang version 11.0.0"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.9.330 K-Nel M1721 Release 1.2"
+#define UTS_RELEASE "4.9.330 K-Nel M1721 Release 1.2-g3a7d65f3f-dirty"
