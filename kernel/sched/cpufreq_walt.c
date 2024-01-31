d41fd65139d836aa8326792ca3677ee75f082461
kanon-ify <ributkukuh270@gmail.com>
2024-01-31 14:55:02 +0700

    cpufreq_walt: added on defconfig and cleanup

--- kernel/sched/cpufreq_walt.c
+++ kernel/sched/cpufreq_walt.c
@@ -1016,9 +1016,6 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct waltgov_policy *wg_policy;
 	unsigned int rate_limit_us;
 
-	if (task_is_booster(current))
-		return count;
-
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -1038,9 +1035,6 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct waltgov_tunables *tunables = to_waltgov_tunables(attr_set);
 	struct waltgov_policy *wg_policy;
 	unsigned int rate_limit_us;
-
-	if (task_is_booster(current))
-		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
