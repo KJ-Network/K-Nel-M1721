28b5be321d57ae2a4dfd5666243cc69f16efb427
KNKJ55 <kskjprotonemail@proton.me>
2024-02-25 07:53:22 +0800

    update new 1.3 source

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ CROSS_COMPILE=aarch64-linux-android- \
 CROSS_COMPILE_ARM32=arm-linux-androideabi- \
 CLANG_TRIPLE=aarch64-linux-gnu- "
 
+#build without zram
+make ${args} m1721_without_zram_defconfig
+#make ${args} xxxx_defconfig
+make ${args} 2>&1 | tee build-without-zram.log
+
+mv out/arch/arm64/boot/Image.gz out/arch/arm64/boot/Image-Without-Zram.gz
 
 #build
 make ${args} m1721_defconfig
@@ -22,7 +28,10 @@ make ${args} 2>&1 | tee build.log
 echo '----- 编译结束！ -----'
 echo '----- 开始打包······ -----'
 cd Anykernel3
-cp ../out/arch/arm64/boot/Image.gz ./
+mv ../out/arch/arm64/boot/Image.gz ./
 zip -r '../K-Nel M1721.zip' *
+rm Image.gz
+mv ../out/arch/arm64/boot/Image-Without-Zram.gz ./Image.gz
+zip -r '../K-Nel M1721 Without Zram.zip' *
 echo '----- 打包结束！ -----'
 echo '内核已在当前目录下自动打包，文件名“K-Nel M1721.zip”'
--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#3 SMP PREEMPT Sat Feb 24 16:09:47 CST 2024"
+#define UTS_VERSION "#11 SMP PREEMPT Sun Feb 25 07:20:36 CST 2024"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "localhost"
 #define LINUX_COMPILER "clang version 11.0.0"
