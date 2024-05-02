ce432ae5b30c4516952b48c2c453e4cddc830f0e
KNKJ55 <knkj55@proton.me>
2024-05-02 10:23:35 +0800

    Automatically update source code using scripts

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
-echo '----- 编译开始······ -----'
+#!/usr/bin/bash
+echo '----- Build Running······ -----'
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH="/root/Toolchain/clang-r399163b/bin:/root/Toolchain/gcc64/bin:/root/Toolchain/gcc32/bin:$PATH"
+export PATH="/root/K-Nel-Projects/Toolchain/clang-r399163b/bin:/root/K-Nel-Projects/Toolchain/gcc64/bin:/root/K-Nel-Projects/Toolchain/gcc32/bin:$PATH"
 
 args="-j6 \
 ARCH=arm64 \
@@ -16,30 +16,11 @@ CLANG_TRIPLE=aarch64-linux-gnu- "
 #build
 make ${args} m1721_defconfig
 #make ${args} xxxx_defconfig
-make ${args} 2>&1 | tee build-kernel.log
+make ${args} 2>&1
 
 if [ -s out/arch/arm64/boot/Image.gz ]
-then echo '----- 编译结束！ -----'
+then echo '----- Build Finished! -----'
 else
-echo '----- 编译失败！-----'
+echo '----- Build Failed! -----'
 exit
 fi
-
-echo '----- 开始打包······ -----'
-if [ -s out/arch/arm64/boot/Image.gz ]
-then cd Anykernel3
-else
-echo '----- 打包失败！-----'
-exit
-fi
-mv ../out/arch/arm64/boot/Image.gz ./
-if [ -e '../K-Nel-M1721.zip' ]
-then rm '../K-Nel-M1721.zip'
-fi
-zip -r '../K-Nel-M1721.zip' *
-if [ -s '../K-Nel-M1721.zip' ]
-then test
-else exit
-fi
-echo '----- 打包结束！ -----'
-echo '内核已在当前目录下自动打包，名为“K-Nel-M1721.zip”'
--- update-sources-to-github.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-if [ -s out ]
-then rm -rf out
-fi
-git submodule add https://github.com/tiann/kernelsu.git KernelSU
-git add .
-git commit -m "Automatically update source code using scripts"
-git push origin Kernel
