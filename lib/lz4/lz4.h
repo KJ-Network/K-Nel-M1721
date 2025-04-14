e172fbc66b086ec01236a859eecf53ae283bf86f
Juhyung Park <qkrwngud825@gmail.com>
2025-04-14 16:13:49 -0700

    lz4: fix LZ4_compress_fast() definition
    LZ4_compress_fast() should be exported with wrkmem.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: I89c1b91f7b84782fd3125a169c9c8dec758bb3f4
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- lib/lz4/lz4.h
+++ lib/lz4/lz4.h
@@ -274,7 +274,7 @@ LZ4LIB_API int LZ4_compressBound(int inputSize);
     Values > LZ4_ACCELERATION_MAX will be replaced by LZ4_ACCELERATION_MAX (currently == 65537, see lz4.c).
 */
 LZ4LIB_API int LZ4_compress_fast(const char *src, char *dst, int srcSize,
-				 int dstCapacity, int acceleration);
+				 int dstCapacity, int acceleration, void *wrkmem);
 
 /*! LZ4_compress_fast_extState() :
  *  Same as LZ4_compress_fast(), using an externally allocated memory space for its state.
